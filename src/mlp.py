a5cb52610d3875d6baf883f892c18e329215137c
Samuel Aguilar-Lussier <sam.aglussier@gmail.com>
2022-03-18 10:58:36 -0400

    Model overfits, needs to be regularized- trainloss:0.09

--- src/mlp.py
+++ src/mlp.py
@@ -136,7 +136,7 @@ validation_set, test_set = split_dataset(validation_set, split=0.9)
 
 mlp_model = MLP(240, 1)
 
-train(mlp_model, train_set, validation_set, epochs=150, learning_rate=0.005, batch_size=1, loss_func=nn.MSELoss(), device=DEVICE)
+train(mlp_model, train_set, validation_set, epochs=50, learning_rate=0.0005, batch_size=1, loss_func=nn.MSELoss(), device=DEVICE)
 torch.save(mlp_model.state_dict(), './mlp_model.pt')
 
 mlp_model.load_state_dict(torch.load('./mlp_model.pt'))
