86bf7a8307dab59a65ad35928dd8671e47db5f69
Ileumassamuel <53974949+Ileumassamuel@users.noreply.github.com>
2022-03-28 21:28:39 -0400

    cash

--- src/dataset_generator.py
+++ src/dataset_generator.py
@@ -1,7 +1,12 @@
 from typing import List
 import random
 import numpy as np
+import pandas as pd
 from nupack import Strand, Complex, ComplexSet, Model, SetSpec, complex_analysis
+import torch
+from torch.utils.data import TensorDataset
+from encoding import oneHotEncodeMany
+
 
 
 def generateSequence(length: int):
@@ -35,6 +40,23 @@ def getFreeEnergy(sequences: List[str]):
     energies = [results[comp].mfe[0].energy for comp in complexes]
 
     return energies
+
+def loadDataset(fileName="variable_length_dataset.csv", encoding = 'onehot'):
+    data = pd.read_csv(fileName, header=None, delimiter=',', dtype={0: str, 1: float}) # type: ignore
+
+    numpified = data.to_numpy() # type: ignore
+    sequences = numpified[:, 0].astype(str)
+    energies = numpified[:, 1].astype(float)
+
+    x = None
+
+    if encoding == 'onehot':
+        x = oneHotEncodeMany(sequences)
+
+    xTensor= torch.tensor(x, dtype=torch.float32)
+    yTensor = torch.tensor(energies, dtype=torch.float32)
+
+    return TensorDataset(xTensor, yTensor)
 
 
 if __name__ == "__main__":
--- src/traditional.py
+++ src/traditional.py
@@ -2,3 +2,6 @@ import numpy as np
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVR
+
+if __name__ == "__main__":
+    pass
