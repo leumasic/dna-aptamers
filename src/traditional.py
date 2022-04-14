a73f57794b314d7d076090dcaa73340c13d9165c
Ileumassamuel <53974949+Ileumassamuel@users.noreply.github.com>
2022-04-14 15:18:35 -0400

    Further testing

--- src/traditional.py
+++ src/traditional.py
@@ -6,7 +6,8 @@ from dataset_generator import loadCsvDataset
 from encoding import oneHotEncodeMany, frequencyEncodeMany
 
 if __name__ == "__main__":
-    X, y = loadCsvDataset("./1mill_dataset.csv", frequencyEncodeMany, (oneHotEncodeMany, { 'seqLength' : 40 }))
+    X, y = loadCsvDataset("./1mill_dataset.csv", frequencyEncodeMany, (oneHotEncodeMany, { 'seqLength' :  60 }))
+    coolX, coolY = loadCsvDataset("./vlad.csv", frequencyEncodeMany, (oneHotEncodeMany, { 'seqLength' :  60 }))
     xTrain, xTest, yTrain, yTest = train_test_split(X, y, test_size=0.2, random_state=42)
 
     linModels: dict[str, LinearModel] = {
@@ -17,12 +18,19 @@ if __name__ == "__main__":
     }
 
     for name, reg in linModels.items():
-        reg.fit(xTrain, yTrain)
+        reg.fit(X, y)
 
-        yTrainPred = reg.predict(xTrain)
-        trainRmse = mean_squared_error(yTrain, yTrainPred, squared=False)
-
-        yTestPred = reg.predict(xTest)
-        testRmse = mean_squared_error(yTest, yTestPred, squared=False)
-
-        print(name, trainRmse, testRmse)
+        yPred = reg.predict(coolX)
+        rmse = mean_squared_error(coolY, yPred, squared=False)
+        print(name, rmse)
+    
+    # for name, reg in linModels.items():
+    #     reg.fit(xTrain, yTrain)
+    #
+    #     yTrainPred = reg.predict(xTrain)
+    #     trainRmse = mean_squared_error(yTrain, yTrainPred, squared=False)
+    #
+    #     yTestPred = reg.predict(xTest)
+    #     testRmse = mean_squared_error(yTest, yTestPred, squared=False)
+    #
+    #     print(name, trainRmse, testRmse)
