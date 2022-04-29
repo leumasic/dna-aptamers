d5211cb9e5907b36da2c7394f63341bd6d52cd07
Ileumassamuel <53974949+Ileumassamuel@users.noreply.github.com>
2022-04-29 08:55:38 -0400

    Added mape error

--- src/traditional.py
+++ src/traditional.py
@@ -1,13 +1,12 @@
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression, Lasso, Ridge, ElasticNet
 from sklearn.linear_model._base import LinearModel
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
 from dataset_generator import loadCsvDataset
 from encoding import oneHotEncodeMany, frequencyEncodeMany
 
 if __name__ == "__main__":
     X, y = loadCsvDataset("./1mill_dataset.csv", frequencyEncodeMany, (oneHotEncodeMany, { 'seqLength' :  60 }))
-    coolX, coolY = loadCsvDataset("./vlad.csv", frequencyEncodeMany, (oneHotEncodeMany, { 'seqLength' :  60 }))
     xTrain, xTest, yTrain, yTest = train_test_split(X, y, test_size=0.2, random_state=42)
 
     linModels: dict[str, LinearModel] = {
@@ -18,19 +17,13 @@ if __name__ == "__main__":
     }
 
     for name, reg in linModels.items():
-        reg.fit(X, y)
+        reg.fit(xTrain, yTrain)
 
-        yPred = reg.predict(coolX)
-        rmse = mean_squared_error(coolY, yPred, squared=False)
-        print(name, rmse)
-    
-    # for name, reg in linModels.items():
-    #     reg.fit(xTrain, yTrain)
-    #
-    #     yTrainPred = reg.predict(xTrain)
-    #     trainRmse = mean_squared_error(yTrain, yTrainPred, squared=False)
-    #
-    #     yTestPred = reg.predict(xTest)
-    #     testRmse = mean_squared_error(yTest, yTestPred, squared=False)
-    #
-    #     print(name, trainRmse, testRmse)
+        yTrainPred = reg.predict(xTrain)
+        trainRmse = mean_squared_error(yTrain, yTrainPred, squared=False)
+
+        yTestPred = reg.predict(xTest)
+        testRmse = mean_squared_error(yTest, yTestPred, squared=False)
+        testMape = mean_absolute_percentage_error(yTest, yTestPred)
+
+        print(name, trainRmse, testRmse, testMape)
